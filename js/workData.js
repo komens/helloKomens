9238c4a8e7257b21ba1a2e0470b4ed1a227695e6
控梦师 <37606163+komens@users.noreply.github.com>
2019-07-27 21:49:03 +0800

    Add files via upload

---
+++ js/workData.js
@@ -0,0 +1,81 @@
+const workData = [
+    [
+        {
+            title: "去哪儿网",
+            url: "javascript:void(0)",
+            intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+            env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+            imgUrl: "http://pic1.win4000.com/mobile/2019-07-23/5d36c2c984dd5.jpg",
+            imgType: "img1"
+        }, {
+            title: "去哪儿网",
+            url: "javascript:void(0)",
+            intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+            env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+            imgUrl: "http://pic1.win4000.com/mobile/2019-07-23/5d36c2cb56142.jpg",
+            imgType: "img1"
+        }, {
+            title: "去哪儿网",
+            url: "javascript:void(0)",
+            intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+            env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+            imgUrl: "http://pic1.win4000.com/mobile/2019-07-23/5d36c2ca7519c.jpg",
+            imgType: "img1"
+        }
+    ],
+    [
+      {
+        title: "去哪儿网",
+        url: "javascript:void(0)",
+        intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+        env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+        imgUrl: "//bpic.588ku.com/photo_pic/19/07/11/5588ea655503eb13678b47cd9430aa2a.jpg",
+        imgType: "img2"
+      }, {
+          title: "去哪儿网",
+          url: "javascript:void(0)",
+          intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+          env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+          imgUrl: "http://pic1.win4000.com/mobile/2019-07-15/5d2c3ccee4c2e.jpg",
+          imgType: "img1"
+      }, {
+          title: "去哪儿网",
+          url: "javascript:void(0)",
+          intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+          env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+          imgUrl: "http://pic1.win4000.com/mobile/2019-07-15/5d2c3cd0671e4.jpg",
+          imgType: "img1"
+      }
+    ],
+    [
+      {
+        title: "去哪儿网",
+        url: "javascript:void(0)",
+        intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+        env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+        imgUrl: "http://pic1.win4000.com/mobile/2019-07-05/5d1f125fc4fee.jpg",
+        imgType: "img1"
+      }, {
+        title: "去哪儿网",
+        url: "javascript:void(0)",
+        intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+        env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+        imgUrl: "//bpic.588ku.com/back_list_pic/19/06/20/3244bf79665aef3dd8361ba4fbb49f1c.jpg",
+        imgType: "img2"
+      }, {
+        title: "去哪儿网",
+        url: "javascript:void(0)",
+        intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+        env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+        imgUrl: "http://pic1.win4000.com/mobile/2019-07-05/5d1f1261344eb.jpg",
+        imgType: "img1"
+      }, {
+        title: "去哪儿网",
+        url: "javascript:void(0)",
+        intro: "该项目先用纯css编写了静态的页面，再使用vue2.x来进行项目的开发；通过vue-router来实现单页面的切换；使用第三方库better-scroll来完成页面滑动，优化用户的体验；使用MVVM模式进行模块化编程。数据库采用第三方的API，用nginx做反向代理，使用阿里云服务器进行项目上线。用户可以通过该App实时查询最新上线的电影情况以及最火爆的电影详情，还可以搜索相关的电影，还能定位当前的城市位置，获得影院相关信息。目前用户还不能注册和登陆，相关功能请等待后续开发...",
+        env: "node v12.4.0 | npm 6.9.0 | vue-cli 3.4.1",
+        imgUrl: "//bpic.588ku.com/back_list_pic/19/04/17/a33ee436157efc3fd9b35fa4a11fe097.jpg",
+        imgType: "img2"
+      }
+    ]
+]
